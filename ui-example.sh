ec941ef08c4ac1e4cc43926b8de972dafb6f61ce
Ian McDonald <irm@pdx.edu>
2024-08-25 08:45:49 -0700

    Adding more notes about read (commit #3)

--- ui-example.sh
+++ ui-example.sh
@@ -1,4 +1,4 @@
-# 'read' command requests user to input text
 echo "¿como te llamas?"
 read nombre
 echo "Hola $nombre"
+#the read command requests user input, we use `$` to recall the variable established by `read`
