9213deea659001ee427f1a47256fd08d0b24c44b
Ian McDonald <irm@pdx.edu>
2024-09-05 21:07:11 -0700

    Commit again

--- ui-example.sh
+++ ui-example.sh
@@ -2,3 +2,6 @@ echo "¿como te llamas?"
 read nombre
 echo "Hola $nombre"
 #the read command requests user input, we use `$` to recall the variable established by `read`
+
+
+#The past is never dead. It's not even past.
